294b3be21c7ee8a28310bd2fda67a3ea2891b9a4
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2022-02-26 12:47:17 -0800

    @create => static route to 'static#home'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'static/home'
+  get 'static/about'
+  get 'static/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'home#static'
 end
